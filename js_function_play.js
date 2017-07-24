1a066cbbcbddd8c4f2c1858e002ebb507c28b626
FALLENau <reece.c.jones@gmail.com>
2017-07-24 14:19:35 +0100

    js func

---
+++ js_function_play.js
@@ -0,0 +1,11 @@
+// function hello(){
+//   console.log("Hey Everyone!")
+// }
+//
+// hello()
+//
+function mood(name, mood){
+  return name + " you are " + mood
+}//if return is removed you will get a "undefined" error in the console.log
+
+console.log("the return value of mood is: ", mood("Zsolt", "in greate pain"))
