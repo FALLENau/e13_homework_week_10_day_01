c41edb3d538e3a12758a80c83ccca82879e1198e
FALLENau <reece.c.jones@gmail.com>
2017-07-24 13:44:29 +0100

    pets loops js

--- control_float.js
+++ control_float.js
@@ -10,9 +10,24 @@
 // else {
 //   console.log("Hello", myName)
 // }
-
-var pet = "cat"
-
-switch (pet) {
-  
-}
+//
+// var pet = "cat"
+//
+// switch (pet) {
+//   case "cat":
+//     console.log("sorf kitty, warm kitty, little ball of fur")
+//     break
+//   case "dog":
+//     console.log("who let the dogs out")
+//     break
+//   default:
+//     console.log("No pets, sad")
+// }
+//
+// console.log(1 + 1 === 2 ? "yay, maths" : "maths is broken")
+// 
+// if (1 + 1 === 2) {
+//   "yay", "maths"
+// } esle {
+//   "maths is broken"
+// }
---
+++ loops.js
@@ -0,0 +1,5 @@
+var pets = ["cat", "dog", "pika"]
+
+for (var pet in pets) {
+  console.log(pets[pet])
+}
