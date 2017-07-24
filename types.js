2c281840944db1c9f073f9cf086c7b269927467b
FALLENau <reece.c.jones@gmail.com>
2017-07-24 11:26:46 +0100

    types of js

---
+++ types.js
@@ -0,0 +1,17 @@
+// var myString = "a string"
+// console.log(myString)
+
+
+// var a = 6
+// var b = 2
+// var c = a / b
+
+// Math.floor()
+// Math.ceil()
+
+// var myString = "a nice piece of text"
+// myString.lenght
+// myString.toUpperCase()
+
+true
+false
