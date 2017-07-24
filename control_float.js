54dc1d5a8b9cab82d8832657f5c81949079ef250
FALLENau <reece.c.jones@gmail.com>
2017-07-24 13:31:14 +0100

    control

---
+++ control_float.js
@@ -0,0 +1,18 @@
+//Basic sums
+// var myName = "Reece"
+//
+// if (myName === "Reece") {
+//   console.log("Yo Reece")
+// }
+// else if (myName === "Zsolt") {
+//   console.log("Zsolt...")
+// }
+// else {
+//   console.log("Hello", myName)
+// }
+
+var pet = "cat"
+
+switch (pet) {
+  
+}
